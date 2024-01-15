bb7f7f0405594caf9eb452e7fdec3ea6acaf82f0
AASIF RAZA <50637795+razaaasif@users.noreply.github.com>
2024-01-15 19:03:55 +0530

    added project

---
+++ caveatemptor-auction-site/src/main/java/com/raza/entity/User.java
@@ -0,0 +1,25 @@
+package com.raza.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1143880903673683863L;
+	protected String username;
+
+	public User() {
+	}
+
+	public String getUserName() {
+		return this.username;
+	}
+
+	public void serUserName(String username) {
+		this.username = username;
+	}
+
+	public BigDecimal calcShippingCosts(Address fromLocation) {
+		return null;
+	}
+
+}
